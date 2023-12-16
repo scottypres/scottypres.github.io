4e8810e9208607515b089dfe2307c18576250eb4
scottypres <50996584+scottypres@users.noreply.github.com>
2023-12-16 01:22:59 -0500

    Revert "Test 2"
    This reverts commit ed340fb2f9ebd32a80b4ace5df2f77744c1ecb12.

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-let latitude, longitude;
-
-document.addEventListener('DOMContentLoaded', function () {
-    const input = document.getElementById('autocomplete');
-
-    input.addEventListener('input', function() {
-        if (this.value.length > 2) { // To minimize requests, waiting for at least 3 characters to start autocompletion
-            getLocationSuggestions(this.value);
-        }
-    });
-});
-
-function getLocationSuggestions(query) {
-    const apiKey = 'pk.56b37a44389aea335d1c84d58086743d';
-    const url = `https://api.locationiq.com/v1/autocomplete.php?key=${apiKey}&q=${encodeURIComponent(query)}`;
-
-    axios.get(url)
-        .then(response => {
-            if (response.data && response.data[0]) {
-                // Just to demonstrate, we pick the first result as the user's location.
-                latitude = response.data[0].lat;
-                longitude = response.data[0].lon;
-                updateCoordinates(latitude, longitude);
-            }
+document.addEventListener("DOMContentLoaded", function() {
+    const apiUrl = 'https://api.locationiq.com/v1/autocomplete.php';
+    let searchBox = document.getElementById('location-input');
+    let autoCompleteResults = document.getElementById('autocomplete-list');
+  
+    searchBox.addEventListener('input', function() {
+      let searchTerm = this.value;
+  
+      if (!searchTerm) {
+        autoCompleteResults.innerHTML = '';
+        return;
+      }
+  
+      fetch(`${apiUrl}?key=pk.56b37a44389aea335d1c84d58086743d&q=${encodeURIComponent(searchTerm)}&limit=5`)
+        .then(response => response.json())
+        .then(data => {
+          autoCompleteResults.innerHTML = '';
+          data.forEach(function(item) {
+            let div = document.createElement('div');
+            div.textContent = item.display_name;
+            div.addEventListener('click', function() {
+              searchBox.value = item.display_name;
+              autoCompleteResults.innerHTML = '';
+              document.getElementById('latitude').value = item.lat;
+              document.getElementById('longitude').value = item.lon;
+            });
+            autoCompleteResults.appendChild(div);
+          });
         })
         .catch(error => {
-            console.error('An error occurred with the LocationIQ API:', error);
+          console.error('Error:', error);
         });
-}
-
-function updateCoordinates(lat, lon) {
-    console.log(`Latitude: ${lat}, Longitude: ${lon}`); // You can handle coordinates as you need.
-    // If needed, update these to the UI or send to a server, etc.
-}
+    });
+  
+    document.addEventListener('click', function(e) {
+      if (e.target.id !== 'location-input') {
+        autoCompleteResults.innerHTML = '';
+      }
+    });
+  });
