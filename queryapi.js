930fed122e905b6513be4d3e2c51ab5ab77aaf8f
scottypres <50996584+scottypres@users.noreply.github.com>
2023-12-16 18:30:41 -0500

    converted labels

--- queryapi.js
+++ queryapi.js
@@ -4,7 +4,17 @@ let GFSData;
 
 const iconApiUrl = 'https://api.open-meteo.com/v1/dwd-icon';
 const openMeteoApiUrl = 'https://api.open-meteo.com/v1/forecast';
-
+const hPaToFt = {
+    "1000hPa": "361ft",
+    "975hPa": "1050ft",
+    "950hPa": "1640ft",
+    "925hPa": "2625ft",
+    "900hPa": "3281ft",
+    "850hPa": "4921ft",
+    "800hPa": "6234ft","700hPa": "9842ft","600hPa": "13770ft"
+  };
+  
+const meterToFeet = 3.28084;
 
 const getWindsButton = document.getElementById('queryApiButton');
 getWindsButton.addEventListener('click', async function () {
@@ -13,14 +23,20 @@ getWindsButton.addEventListener('click', async function () {
     if (lat && lon) {
         try {
              iconData = await getIconData(lat, lon);
-            console.log("ICON Data:", iconData);
+            const convertedIconData = convertData(iconData);
+            openMeteoData = await getOpenMeteoModelData(lat, lon);
+            const convertedOpenMeteoData = convertData(openMeteoData);
+             GFSData = await getGFSData(lat, lon);
+            const convertedGFSData = convertData(GFSData);
             
 
-             openMeteoData = await getOpenMeteoModelData(lat, lon);
-            console.log("OpenMeteo Data:", openMeteoData);
+           
 
-             GFSData = await getGFSData(lat, lon);
-            console.log("GFS Data:", GFSData);
+
+// Now you can use the converted data to update the UI or tables
+console.log('Converted ICON Data:', convertedIconData);
+console.log('Converted OpenMeteo Data:', convertedOpenMeteoData);
+console.log('Converted GFS Data:', convertedGFSData);
 
             // Process the data here and update the table or display it in the UI
         } catch (error) {
@@ -47,54 +63,82 @@ async function getIconData(latitude, longitude) {
     const url = `${iconApiUrl}?latitude=${latitude}&longitude=${longitude}` +
     `&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,weather_code,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,wind_speed_10m,wind_speed_80m,wind_speed_180m,wind_direction_10m,wind_direction_80m,wind_direction_180m,wind_gusts_10m,temperature_80m,temperature_180m,temperature_1000hPa,temperature_975hPa,temperature_950hPa,temperature_925hPa,temperature_900hPa,temperature_850hPa,temperature_800hPa,temperature_700hPa,temperature_600hPa,cloud_cover_1000hPa,cloud_cover_975hPa,cloud_cover_950hPa,cloud_cover_925hPa,cloud_cover_900hPa,cloud_cover_850hPa,cloud_cover_800hPa,cloud_cover_700hPa,cloud_cover_600hPa,windspeed_1000hPa,windspeed_975hPa,windspeed_950hPa,windspeed_925hPa,windspeed_900hPa,windspeed_850hPa,windspeed_800hPa,windspeed_700hPa,windspeed_600hPa,winddirection_1000hPa,winddirection_975hPa,winddirection_950hPa,winddirection_925hPa,winddirection_900hPa,winddirection_850hPa,winddirection_800hPa,winddirection_700hPa,winddirection_600hPa,cape&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=7`;
     const response = await fetch(url);
-    return response.json();
-    console.log(data); // This will show you the entire structure of the response
+
+    // Then convert the response to JSON
+    const jsonResponse = await response.json();
+
+    // Format and return the API response data
+    return formatApiResponse(jsonResponse);
 }
 
 async function getOpenMeteoModelData(latitude, longitude) {
     const url = `${openMeteoApiUrl}?latitude=${latitude}&longitude=${longitude}` +
     `&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,precipitation_probability,weather_code,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,wind_speed_10m,wind_speed_80m,wind_speed_180m,wind_direction_10m,wind_direction_80m,wind_direction_180m,wind_gusts_10m,temperature_80m,temperature_180m,temperature_1000hPa,temperature_975hPa,temperature_950hPa,temperature_925hPa,temperature_900hPa,temperature_850hPa,temperature_800hPa,temperature_700hPa,temperature_600hPa,cloud_cover_1000hPa,cloud_cover_975hPa,cloud_cover_950hPa,cloud_cover_925hPa,cloud_cover_900hPa,cloud_cover_850hPa,cloud_cover_800hPa,cloud_cover_700hPa,cloud_cover_600hPa,windspeed_1000hPa,windspeed_975hPa,windspeed_950hPa,windspeed_925hPa,windspeed_900hPa,windspeed_850hPa,windspeed_800hPa,windspeed_700hPa,windspeed_600hPa,winddirection_1000hPa,winddirection_975hPa,winddirection_950hPa,winddirection_925hPa,winddirection_900hPa,winddirection_850hPa,winddirection_800hPa,winddirection_700hPa,winddirection_600hPa,cape&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=14`;
     const response = await fetch(url);
-    return response.json();
-    console.log(data); // This will show you the entire structure of the response
+
+    // Then convert the response to JSON
+    const jsonResponse = await response.json();
+
+    // Format and return the API response data
+    return formatApiResponse(jsonResponse);
 }
 async function getGFSData(latitude, longitude) {
     const url = `https://api.open-meteo.com/v1/gfs?latitude=${latitude}&longitude=${longitude}` +
                 `&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,precipitation_probability,weather_code,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,wind_speed_10m,wind_speed_80m,wind_speed_180m,wind_direction_10m,wind_direction_80m,wind_direction_180m,wind_gusts_10m,temperature_80m,temperature_180m,temperature_1000hPa,temperature_975hPa,temperature_950hPa,temperature_925hPa,temperature_900hPa,temperature_850hPa,temperature_800hPa,temperature_700hPa,temperature_600hPa,cloud_cover_1000hPa,cloud_cover_975hPa,cloud_cover_950hPa,cloud_cover_925hPa,cloud_cover_900hPa,cloud_cover_850hPa,cloud_cover_800hPa,cloud_cover_700hPa,cloud_cover_600hPa,windspeed_1000hPa,windspeed_975hPa,windspeed_950hPa,windspeed_925hPa,windspeed_900hPa,windspeed_850hPa,windspeed_800hPa,windspeed_700hPa,windspeed_600hPa,winddirection_1000hPa,winddirection_975hPa,winddirection_950hPa,winddirection_925hPa,winddirection_900hPa,winddirection_850hPa,winddirection_800hPa,winddirection_700hPa,winddirection_600hPa,cape&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=14`;
-    const response = await fetch(url);
-    return response.json();
-    console.log(data); // This will show you the entire structure of the response
-}
+                const response = await fetch(url);
+
+                // Then convert the response to JSON
+                const jsonResponse = await response.json();
+            
+                // Format and return the API response data
+                return formatApiResponse(jsonResponse);
+            }
         
 
-function convertHpaToFeetLabels(dataArray) {
-    // Define a mapping from hPa to feet
-    const hPaToFeet = {
-        "1000hPa": "361ft",
-        "975hPa": "1050ft",
-        "950hPa": "1640ft",
-        "925hPa": "2625ft",
-        "900hPa": "3281ft",
-        "850hPa": "4921ft",
-        "800hPa": "6234ft"
-        // Add more mappings if needed
-    };
-
-    dataArray.forEach(data => {
-        const keys = Object.keys(data);
-        keys.forEach(key => {
-            // Check if the key ends with 'hPa'
-            if (key.endsWith('hPa')) {
-                // Check if we have an equivalent feet value for this hPa key
-                const convertedKey = hPaToFeet[key];
-                if (convertedKey) {
-                    // Replace the hPa data with its feet label equivalent
-                    data[convertedKey] = data[key];
-                    // You may choose to remove the original hPa key or keep it
-                    delete data[key];
-                }
+// New function to reformat the API response data into an array of objects
+function formatApiResponse(apiResponse) {
+    const hourlyData = apiResponse.hourly;
+    const timeSeries = hourlyData.time;
+    const formattedData = timeSeries.map((time, index) => {
+        let dataEntry = { time };
+        for (const property in hourlyData) {
+            if (property !== "time") { // Exclude the time array
+                dataEntry[property] = hourlyData[property][index]; // Match other property arrays with time
             }
-        });
+        }
+        return dataEntry;
     });
+    return formattedData;
 }
+function convertData(data) {
+    const convertedData = [];
 
+    for (const entry of data) {
+        const convertedEntry = {};
+        
+        for (const key in entry) {
+            // Check if the key represents hPa data and convert to feet using provided mapping
+            const hPaMatch = key.match(/(\d+)hPa/);
+            if (hPaMatch && hPaToFt[`${hPaMatch[1]}hPa`]) {
+                const newKey = `${hPaToFt[`${hPaMatch[1]}hPa`].replace('ft', '')}ft`;
+                convertedEntry[newKey] = entry[key];
+            }
+            // Check if the key represents meters data and convert to feet
+            else if (key.match(/(\d+)m/)) {
+                const newKey = key.replace(/(\d+)m/, (match, meterValue) => {
+                    const feetValue = Math.round(meterValue * meterToFeet);
+                    return `${feetValue}ft`;
+                });
+                convertedEntry[newKey] = entry[key];
+            }
+            // If neither, just copy the key as it is
+            else {
+                convertedEntry[key] = entry[key];
+            }
+        }
+
+        convertedData.push(convertedEntry);
+    }
+
+    return convertedData;
+}
