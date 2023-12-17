53f4012fc71bb9ffb27e7398ead41cd1c30a1b18
scottypres <50996584+scottypres@users.noreply.github.com>
2023-12-17 17:55:31 -0500

    latest

--- queryapi.js
+++ queryapi.js
@@ -37,7 +37,20 @@ const tableIconWind = convertToTableData(convertedIconData);
 const tableOpenMeteoWind = convertToTableData(convertedOpenMeteoData);
 const tableGFSWind = convertToTableData(convertedGFSData);
            
+// After fetching and converting your data
+populateTables(tableIconWind, 'icon');
+populateTables(tableOpenMeteoWind, 'openMeteo');
+populateTables(tableGFSWind, 'gfs');
 
+
+
+// Attach event listeners to buttons within each weather-model div
+document.querySelectorAll('.weather-model').forEach(model => {
+  const modelPrefix = model.id;
+  model.querySelector('.btn-wind').addEventListener('click', () => updateCells(modelPrefix, 'wind'));
+  model.querySelector('.btn-temp').addEventListener('click', () => updateCells(modelPrefix, 'temp'));
+  model.querySelector('.btn-cloud').addEventListener('click', () => updateCells(modelPrefix, 'cloud'));
+});
 
 // Now you can use the converted data to update the UI or tables
 console.log('mph ICON Data:', tableIconWind);
@@ -69,7 +82,7 @@ console.log('mph GFS Data:', tableGFSWind);
         
 async function getIconData(latitude, longitude) {
     const url = `${iconApiUrl}?latitude=${latitude}&longitude=${longitude}` +
-    `&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,weather_code,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,wind_speed_10m,wind_speed_80m,wind_speed_180m,wind_direction_10m,wind_direction_80m,wind_direction_180m,wind_gusts_10m,temperature_80m,temperature_180m,temperature_1000hPa,temperature_975hPa,temperature_950hPa,temperature_925hPa,temperature_900hPa,temperature_850hPa,temperature_800hPa,temperature_700hPa,temperature_600hPa,cloud_cover_1000hPa,cloud_cover_975hPa,cloud_cover_950hPa,cloud_cover_925hPa,cloud_cover_900hPa,cloud_cover_850hPa,cloud_cover_800hPa,cloud_cover_700hPa,cloud_cover_600hPa,windspeed_1000hPa,windspeed_975hPa,windspeed_950hPa,windspeed_925hPa,windspeed_900hPa,windspeed_850hPa,windspeed_800hPa,windspeed_700hPa,windspeed_600hPa,winddirection_1000hPa,winddirection_975hPa,winddirection_950hPa,winddirection_925hPa,winddirection_900hPa,winddirection_850hPa,winddirection_800hPa,winddirection_700hPa,winddirection_600hPa,cape&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=3`;
+    `&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,weather_code,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,wind_speed_10m,wind_speed_80m,wind_speed_180m,wind_direction_10m,wind_direction_80m,wind_direction_180m,wind_gusts_10m,temperature_80m,temperature_180m,temperature_1000hPa,temperature_975hPa,temperature_950hPa,temperature_925hPa,temperature_900hPa,temperature_850hPa,temperature_800hPa,temperature_700hPa,temperature_600hPa,cloud_cover_1000hPa,cloud_cover_975hPa,cloud_cover_950hPa,cloud_cover_925hPa,cloud_cover_900hPa,cloud_cover_850hPa,cloud_cover_800hPa,cloud_cover_700hPa,cloud_cover_600hPa,windspeed_1000hPa,windspeed_975hPa,windspeed_950hPa,windspeed_925hPa,windspeed_900hPa,windspeed_850hPa,windspeed_800hPa,windspeed_700hPa,windspeed_600hPa,winddirection_1000hPa,winddirection_975hPa,winddirection_950hPa,winddirection_925hPa,winddirection_900hPa,winddirection_850hPa,winddirection_800hPa,winddirection_700hPa,winddirection_600hPa,cape&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=1`;
     const response = await fetch(url);
 
     // Then convert the response to JSON
@@ -81,7 +94,7 @@ async function getIconData(latitude, longitude) {
 
 async function getOpenMeteoModelData(latitude, longitude) {
     const url = `${openMeteoApiUrl}?latitude=${latitude}&longitude=${longitude}` +
-    `&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,precipitation_probability,weather_code,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,wind_speed_10m,wind_speed_80m,wind_speed_180m,wind_direction_10m,wind_direction_80m,wind_direction_180m,wind_gusts_10m,temperature_80m,temperature_180m,temperature_1000hPa,temperature_975hPa,temperature_950hPa,temperature_925hPa,temperature_900hPa,temperature_850hPa,temperature_800hPa,temperature_700hPa,temperature_600hPa,cloud_cover_1000hPa,cloud_cover_975hPa,cloud_cover_950hPa,cloud_cover_925hPa,cloud_cover_900hPa,cloud_cover_850hPa,cloud_cover_800hPa,cloud_cover_700hPa,cloud_cover_600hPa,windspeed_1000hPa,windspeed_975hPa,windspeed_950hPa,windspeed_925hPa,windspeed_900hPa,windspeed_850hPa,windspeed_800hPa,windspeed_700hPa,windspeed_600hPa,winddirection_1000hPa,winddirection_975hPa,winddirection_950hPa,winddirection_925hPa,winddirection_900hPa,winddirection_850hPa,winddirection_800hPa,winddirection_700hPa,winddirection_600hPa,cape&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=3`;
+    `&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,precipitation_probability,weather_code,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,wind_speed_10m,wind_speed_80m,wind_speed_180m,wind_direction_10m,wind_direction_80m,wind_direction_180m,wind_gusts_10m,temperature_80m,temperature_180m,temperature_1000hPa,temperature_975hPa,temperature_950hPa,temperature_925hPa,temperature_900hPa,temperature_850hPa,temperature_800hPa,temperature_700hPa,temperature_600hPa,cloud_cover_1000hPa,cloud_cover_975hPa,cloud_cover_950hPa,cloud_cover_925hPa,cloud_cover_900hPa,cloud_cover_850hPa,cloud_cover_800hPa,cloud_cover_700hPa,cloud_cover_600hPa,windspeed_1000hPa,windspeed_975hPa,windspeed_950hPa,windspeed_925hPa,windspeed_900hPa,windspeed_850hPa,windspeed_800hPa,windspeed_700hPa,windspeed_600hPa,winddirection_1000hPa,winddirection_975hPa,winddirection_950hPa,winddirection_925hPa,winddirection_900hPa,winddirection_850hPa,winddirection_800hPa,winddirection_700hPa,winddirection_600hPa,cape&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=1`;
     const response = await fetch(url);
 
     // Then convert the response to JSON
@@ -92,7 +105,7 @@ async function getOpenMeteoModelData(latitude, longitude) {
 }
 async function getGFSData(latitude, longitude) {
     const url = `https://api.open-meteo.com/v1/gfs?latitude=${latitude}&longitude=${longitude}` +
-                `&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,precipitation_probability,weather_code,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,wind_speed_10m,wind_speed_80m,wind_speed_180m,wind_direction_10m,wind_direction_80m,wind_direction_180m,wind_gusts_10m,temperature_80m,temperature_180m,temperature_1000hPa,temperature_975hPa,temperature_950hPa,temperature_925hPa,temperature_900hPa,temperature_850hPa,temperature_800hPa,temperature_700hPa,temperature_600hPa,cloud_cover_1000hPa,cloud_cover_975hPa,cloud_cover_950hPa,cloud_cover_925hPa,cloud_cover_900hPa,cloud_cover_850hPa,cloud_cover_800hPa,cloud_cover_700hPa,cloud_cover_600hPa,windspeed_1000hPa,windspeed_975hPa,windspeed_950hPa,windspeed_925hPa,windspeed_900hPa,windspeed_850hPa,windspeed_800hPa,windspeed_700hPa,windspeed_600hPa,winddirection_1000hPa,winddirection_975hPa,winddirection_950hPa,winddirection_925hPa,winddirection_900hPa,winddirection_850hPa,winddirection_800hPa,winddirection_700hPa,winddirection_600hPa,cape&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=3`;
+                `&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,precipitation_probability,weather_code,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,wind_speed_10m,wind_speed_80m,wind_speed_180m,wind_direction_10m,wind_direction_80m,wind_direction_180m,wind_gusts_10m,temperature_80m,temperature_180m,temperature_1000hPa,temperature_975hPa,temperature_950hPa,temperature_925hPa,temperature_900hPa,temperature_850hPa,temperature_800hPa,temperature_700hPa,temperature_600hPa,cloud_cover_1000hPa,cloud_cover_975hPa,cloud_cover_950hPa,cloud_cover_925hPa,cloud_cover_900hPa,cloud_cover_850hPa,cloud_cover_800hPa,cloud_cover_700hPa,cloud_cover_600hPa,windspeed_1000hPa,windspeed_975hPa,windspeed_950hPa,windspeed_925hPa,windspeed_900hPa,windspeed_850hPa,windspeed_800hPa,windspeed_700hPa,windspeed_600hPa,winddirection_1000hPa,winddirection_975hPa,winddirection_950hPa,winddirection_925hPa,winddirection_900hPa,winddirection_850hPa,winddirection_800hPa,winddirection_700hPa,winddirection_600hPa,cape&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=1`;
                 const response = await fetch(url);
 
                 // Then convert the response to JSON
@@ -184,3 +197,73 @@ function convertToTableData(weatherData) {
     return tableData;
 }
 
+function populateTables(data, modelPrefix) {
+    const thead = document.getElementById(`${modelPrefix}-thead`);
+    const tbody = document.getElementById(`${modelPrefix}-tbody`);
+  
+    // Clear previous table headers and rows
+    thead.innerHTML = '';
+    tbody.innerHTML = '';
+  
+    // Extract unique times and altitudes
+    const times = Object.keys(data);
+    const altitudes = [...new Set(times.map(time => Object.keys(data[time])).flat())].sort();
+  
+    // Create the table headers (times)
+    const headerRow = document.createElement('tr');
+    headerRow.appendChild(document.createElement('th')); // Empty top-left cell
+    times.forEach(time => {
+      const th = document.createElement('th');
+      th.textContent = time;
+      headerRow.appendChild(th);
+    });
+    thead.appendChild(headerRow);
+    
+    // Create the rows (altitudes)
+    altitudes.forEach(altitude => {
+      const row = document.createElement('tr');
+      const altitudeTh = document.createElement('th');
+      altitudeTh.textContent = altitude;
+      row.appendChild(altitudeTh);
+  
+      times.forEach(time => {
+        const cell = document.createElement('td');
+        
+        // Setup a default display value (e.g., wind speed)
+        const windSpeed = data[time][altitude]?.windSpeed || '-';
+        cell.textContent = windSpeed;
+        cell.dataset.type = 'wind';  // Storing the default type
+        
+        // Attach an identifier to each cell for future updates
+        cell.dataset.time = time;
+        cell.dataset.altitude = altitude;
+  
+        row.appendChild(cell);
+      });
+      tbody.appendChild(row);
+    });
+  }
+  
+  // Define a function to update table cells based on the selected data type
+  function updateCells(modelPrefix, dataType) {
+    const tbody = document.getElementById(`${modelPrefix}-tbody`);
+    
+    // Update each cell with the selected data type
+    for (let row of tbody.rows) {
+      for (let cell of row.cells) {
+        // Skip the first cell in the row as it's the altitude header
+        if (cell === row.cells[0]) continue; 
+  
+        // Retrieve time and altitude from data attributes
+        const time = cell.dataset.time;
+        const altitude = cell.dataset.altitude;
+  
+        // Update cell content based on dataType
+        let value = (data[time] && data[time][altitude]) ? data[time][altitude][dataType] : '-';
+        cell.textContent = value;
+  
+        // Update current data type stored in the cell
+        cell.dataset.type = dataType;
+      }
+    }
+  }
